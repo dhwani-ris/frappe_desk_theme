75241b27745c774142ac69afb346445c8bb765c9
Vaishali Sahni <vaishalisahni10@gmail.com>
2026-01-13 18:41:27 +0000

    fix(breadcrumbs): update patch for Frappe v15

--- frappe_desk_theme/hooks.py
+++ frappe_desk_theme/hooks.py
@@ -30,7 +30,7 @@ app_include_css = "/assets/frappe_desk_theme/css/frappe_desk_theme.bundle.css"
 app_include_js = [
 	"/assets/frappe_desk_theme/js/frappe_desk_theme.bundle.js",
 	"/assets/frappe_desk_theme/js/sidebar/sidebar_override.bundle.js",
-	"/assets/frappe_desk_theme/js/sidebar/breadcrumb_override.bundle.js",
+	"/assets/frappe_desk_theme/js/sidebar/breadcrumb_override_patch.js",
 ]
 # include js, css files in header of web template
 web_include_css = "/assets/frappe_desk_theme/css/frappe_desk_theme.bundle.css"
